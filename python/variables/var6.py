9869159a4a051115f2b23f06b47473ddb0f91f57
Ariel Aram (Real-Time Tech College) <ariel.segabinazzi@senai.edu.br>
2026-02-21 16:53:10 -0300

    Omg my teacher is nuts lmao

--- python/variables/var6.py
+++ python/variables/var6.py
@@ -7,16 +7,29 @@
 for num in range(3, 13):
     print(num)
 
+print("======")
+
 for i in range(1, 6):
     print("$" * i)
+
+print("======")
 
 name = "arielaram"
 for i in name:
     print(i)
+
+print("======")
 
 addition = 0
 subtraction = 0
 for i in range(1, 6):
     addition = addition + i
     subtraction = subtraction - i
-    print(f"The result of the addition is {addition}, while the result of the subtraction is {subtraction}")
+    print(
+        f"The result of the addition is {addition}, while the result of the subtraction is {subtraction}"
+    )
+
+print("======")
+
+team = ["Ariel", "Metro", "Aditya", "Bloxy", "Ahmed", "Lore", "Scorpion"]
+print(team)
